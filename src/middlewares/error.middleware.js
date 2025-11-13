60dc42891b4cd5b365dd77bda69e3de467fd0ab5
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2025-11-14 01:04:11 +0700

    chore: add log for error middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,12 +1,15 @@
 const { StatusCodes } = require("http-status-codes");
 
-const HttpErrorHandler = (err, _, res, next) => {
+const HttpErrorHandler = (err, req, res, next) => {
   if (!err) next();
 
   const error = {
     type: err?.cause?.type || "internal_server_error",
     msg: err?.message || "Some error occured on the server.",
   };
+
+  console.log(`Error: [${req.method}] ${req.path}:\n`, error);
+
   res
     .status(err?.cause?.code || StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ error });
