5de9082a2876f823b051c74fbe25872abbf49157
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2025-11-13 12:54:56 +0700

    Initialize project with express

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("common"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
